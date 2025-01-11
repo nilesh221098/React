2b2d1d8e95242927907d64ff0ccce690bd7a2008
nilesh221098 <nileshmishraakbarpur@gmail.com>
2025-01-11 15:33:00 +0530

    update

--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
